6fa871c6e67a94e1e853a955abd7035b56a601cf
Lu <lschwarzman1@hwemail.com>
2026-09-02 13:28:03 -0700

    Update BlockLetters.java

--- BlockLetters.java
+++ BlockLetters.java
@@ -1,14 +1,19 @@
 public class BlockLetters {
     public static void main(String[] args) {
         System.out.print("L   ");
-        System.out.println("    A      SS");
+        System.out.print("    A ");
+        System.out.println("     SS");
         System.out.print("L   ");
-        System.out.println("   A A   SSS");
+        System.out.print("   A A");
+        System.out.println("   SSS");
         System.out.print("L   ");
-        System.out.println("  AAAAA   S");
+        System.out.print("  AAAAA");
+        System.out.println("   S");
         System.out.print("L   ");
-        System.out.println(" A     A  SSS");
+        System.out.print(" A     A");
+        System.out.println("  SSS");
         System.out.print("LLLL");
-        System.out.println(" A     A SSS");
+        System.out.print(" A     A");
+        System.out.println(" SSS");
     }
 }
